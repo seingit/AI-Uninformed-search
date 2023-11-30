2cc817591b0d8b13328682d722fc3c50398885bb
seingit <126335681+seingit@users.noreply.github.com>
2023-12-01 00:19:50 +0330

    Add files via upload

---
+++ maze.py
@@ -0,0 +1,166 @@
+import sys, time
+
+class Node():
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
+
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+
+class Maze():
+
+    def __init__(self, filename):
+        
+
+        # Read file and set height and width of maze
+        with open(filename) as f:
+            contents = f.read()
+
+        # Validate start and goal
+        if contents.count("S") != 1:
+            raise Exception("maze must have exactly one start point")
+        if contents.count("G") != 1:
+            raise Exception("maze must have exactly one goal")
+
+        # Determine height and width of maze
+        contents = contents.splitlines()
+        self.height = len(contents)
+        self.width = max(len(line) for line in contents)
+
+        # Keep track of walls
+        self.walls = []
+        for i in range(self.height):
+            row = []
+            for j in range(self.width):
+                try:
+                    if contents[i][j] == "S":
+                        self.start = (i, j)
+                        row.append(False)
+                    elif contents[i][j] == "G":
+                        self.goal = (i, j)
+                        row.append(False)
+                    elif contents[i][j] == "-":
+                        row.append(False)
+                    else:
+                        row.append(True)
+                except IndexError:
+                    row.append(False)
+            self.walls.append(row)
+            
+        self.solution = None
+
+    # همسایه برمیگردونه و اکشن میده
+    def neighbors(self, state):
+        row, col = state
+        # موقعیت های حرکتو میگه
+        candidates = [
+            ("up", (row - 1, col)),
+            ("down", (row + 1, col)),
+            ("left", (row, col - 1)),
+            ("right", (row, col + 1))
+        ]
+
+        result = []
+        for action, (r, c) in candidates:
+            if 0 <= r < self.height and 0 <= c < self.width and not self.walls[r][c]:
+                result.append((action, (r, c)))
+        return result
+
+
+    def solve(self):
+        """Finds a solution to maze, if one exists."""
+        
+        # Keep track of number of states explored
+        self.num_explored = 0
+
+        # Initialize frontier to just the starting position
+        start = Node(state=self.start, parent=None, action=None)
+        frontier = StackFrontier()
+        frontier.add(start)
+
+        # Initialize an empty explored set
+        self.explored = set()
+
+        # Keep looping until solution found
+        while True:
+
+            # If nothing left in frontier, then no path
+            if frontier.empty():
+                raise Exception("no solution")
+
+            # Choose a node from the frontier
+            node = frontier.remove()
+            self.num_explored += 1
+
+            # If node is the goal, then we have a solution
+            if node.state == self.goal:
+                actions = []
+                cells = []
+                while node.parent is not None:
+                    actions.append(node.action)
+                    cells.append(node.state)
+                    node = node.parent
+                actions.reverse()
+                cells.reverse()
+                self.solution = (actions, cells)
+                return
+
+            # Mark node as explored
+            self.explored.add(node.state)
+
+            # Add neighbors to frontier
+            for action, state in self.neighbors(node.state):
+                if not frontier.contains_state(state) and state not in self.explored:
+                    child = Node(state=state, parent=node, action=action)
+                    frontier.add(child)
+
+
+    def get_solution_path(self):
+        actions, cells = self.solution
+        path = ""
+        for action in actions:
+            if action == "up":
+                path += "U"
+            elif action == "down":
+                path += "D"
+            elif action == "left":
+                path += "L"
+            elif action == "right":
+                path += "R"
+        return path
+
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: python maze.py maze.txt")
+
+m = Maze(sys.argv[1])
+start_time = time.time()
+
+m.solve()
+solution_path = m.get_solution_path()
+print("Solution Path:", solution_path)
+print("States Explored:", m.num_explored - 1)
+
+print('Runtime:', time.time()-start_time)
